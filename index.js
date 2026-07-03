ec7d76bc19ba8278bab01b0c845070a98274bf55
Appana Venkata Jaswanth <appanavenkatajaswanth@Appana-Venkata-Jaswanth-B989s-MacBook-Air.local>
2026-07-03 16:22:37 +0530

    feat/app server

--- index.js
+++ index.js
@@ -18,6 +18,53 @@ app.get("/", (req, res) => {
   res.send(`<h1>Association Server ${userAgent}</h1>`);
   res.end();
 });
+app.get("/home", (req, res) => {
+  res.send(`<h1>Home Page</h1>`);
+  res.end();
+});
+app.get("/about", (req, res) => {
+  res.send(`<h1>About page</h1>`);
+  res.end();
+});
+app.get("/paypoint-merchant-mobile-app", (req, res) => {
+  const ua = req.get("User-Agent") || "";
+  console.log("User Agent", ua);
+  const PLAY_STORE_LINK =
+    "https://play.google.com/store/apps/details?id=com.merchant_mobile_app.paypoint&gl=UK";
+  const APP_STORE_LINK =
+    "https://apps.apple.com/gb/app/paypoint-merchant-mobile-app/id6615070817";
+
+  const downloadLink = /android/i.test(ua)
+    ? PLAY_STORE_LINK
+    : /iPhone|iPad|iPod/i.test(ua)
+      ? APP_STORE_LINK
+      : "https://associationserver.onrender.com";
+  res.send(
+    `<h1>Paypoint merchant mobile app</h1><a href="${downloadLink}">Download</a>`,
+  );
+  res.end();
+});
+app.get("/paypoint-merchant-mobile-app/app", (req, res) => {
+  const ua = req.get("User-Agent") || "";
+  if (/android/i.test(ua)) {
+    return res.redirect(
+      "https://play.google.com/store/apps/details?id=com.merchant_mobile_app.paypoint&gl=UK",
+    );
+  }
+  if (/iPhone|iPad|iPod/i.test(ua)) {
+    return res.redirect(
+      "https://apps.apple.com/gb/app/paypoint-merchant-mobile-app/id6615070817",
+    );
+  }
+
+  return res.redirect("https://associationserver.onrender.com");
+});
+app.use((req, res) => {
+  res.status(404).send(`
+    <h1>404</h1>
+    <h3>Page not found</h3>
+  `);
+});
 
 const port = process.env.PORT || 5000;
 
