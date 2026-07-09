4ab955202c4101fb008c0b1bd4a797455343d465
Appana Venkata Jaswanth <appanavenkatajaswanth@Appana-Venkata-Jaswanth-B989s-MacBook-Air.local>
2026-07-09 15:21:01 +0530

    feat:handepay config

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ configDotenv();
 import express from "express";
 import cors from "cors";
 
+const PAYPOINT_PLAY_STORE_LINK = process.env.PAYPOINT_PLAY_STORE;
+const PAYPOINT_APP_STORE_LINK = process.env.PAYPOINT_APP_STORE;
+const HANDEPAY_PLAY_STORE_LINK = process.env.HANDEPAY_PLAY_STORE;
+const HANDEPAY_APP_STORE_LINK = process.env.HANDEPAY_APP_STORE;
+
 const app = express();
 app.use(cors());
 
@@ -14,7 +19,6 @@ app.use(
 
 app.get("/", (req, res) => {
   const userAgent = req.get("User-Agent");
-  console.log("User Agent", userAgent);
   res.send(`<h1>Association Server ${userAgent}</h1>`);
 });
 app.get("/home", (req, res) => {
@@ -23,37 +27,74 @@ app.get("/home", (req, res) => {
 app.get("/about", (req, res) => {
   res.send(`<h1>About page</h1>`);
 });
+
+app.get("/merchant-mobile-app", (req, res) => {
+  const ua = req.get("User-Agent") || "";
+
+  const platform = /android/i.test(ua)
+    ? "android"
+    : /iPhone|iPad|iPod/i.test(ua)
+      ? "iOS"
+      : "other";
+
+  if (platform === "android")
+    res.send(
+      `<h1 style="color:skyblue;background-color:black;width:fit-content;padding:10px;border-radius:10px">Handepay merchant mobile app</h1><a style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue"  href="${HANDEPAY_PLAY_STORE_LINK}">Download</a>`,
+    );
+  else if (platform === "iOS") {
+    res.send(
+      `<h1 style="color:skyblue;background-color:black;width:fit-content;padding:10px;border-radius:10px">Handepay merchant mobile app</h1><a style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue" href="${HANDEPAY_APP_STORE_LINK}">Download</a>`,
+    );
+  } else
+    res.send(
+      `<h1 style="color:skyblue;background-color:black;width:fit-content;padding:10px;border-radius:10px">Handepay merchant mobile app</h1><a style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue" href="${HANDEPAY_PLAY_STORE_LINK}">Download Android app</a><br/><a style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue" href="${HANDEPAY_APP_STORE_LINK}">Download iOS app</a>`,
+    );
+});
+
+app.use("/merchant-mobile-app/app", (req, res) => {
+  const ua = req.get("User-Agent") || "";
+  if (/android/i.test(ua)) {
+    return res.redirect(HANDEPAY_PLAY_STORE_LINK);
+  }
+  if (/iPhone|iPad|iPod/i.test(ua)) {
+    return res.redirect(HANDEPAY_APP_STORE_LINK);
+  }
+
+  return res.redirect("/merchant-mobile-app");
+});
+
 app.get("/paypoint-merchant-mobile-app", (req, res) => {
   const ua = req.get("User-Agent") || "";
-  console.log("User Agent", ua);
-  const PLAY_STORE_LINK =
-    "https://play.google.com/store/apps/details?id=com.merchant_mobile_app.paypoint&gl=UK";
-  const APP_STORE_LINK =
-    "https://apps.apple.com/gb/app/paypoint-merchant-mobile-app/id6615070817";
 
-  const downloadLink = /android/i.test(ua)
-    ? PLAY_STORE_LINK
+  const platform = /android/i.test(ua)
+    ? "android"
     : /iPhone|iPad|iPod/i.test(ua)
-      ? APP_STORE_LINK
-      : "https://associationserver.onrender.com";
-  res.send(
-    `<h1>Paypoint merchant mobile app</h1><a href="${downloadLink}">Download</a>`,
-  );
+      ? "iOS"
+      : "other";
+
+  if (platform === "android")
+    res.send(
+      `<h1 style="color:yellow;background-color:black;width:fit-content;padding:10px;border-radius:10px">Paypoint merchant mobile app</h1><a style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue" href="${PAYPOINT_PLAY_STORE_LINK}">Download</a>`,
+    );
+  else if (platform === "iOS") {
+    res.send(
+      `<h1 style="color:yellow;background-color:black;width:fit-content;padding:10px;border-radius:10px">Paypoint merchant mobile app</h1><a style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue" href="${PAYPOINT_APP_STORE_LINK}">Download</a>`,
+    );
+  } else
+    res.send(
+      `<h1 style="color:yellow;background-color:black;width:fit-content;padding:10px;border-radius:10px">Paypoint merchant mobile app</h1><a  style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue" href="${PAYPOINT_PLAY_STORE_LINK}">Download Android app</a><br/><a style="padding:20px;border-radius:10px;display:block;width:fit-content;color:white;text-decoration:none;background-color:skyblue" href="${PAYPOINT_APP_STORE_LINK}">Download iOS app</a>`,
+    );
 });
 app.use("/paypoint-merchant-mobile-app/app", (req, res) => {
   const ua = req.get("User-Agent") || "";
   if (/android/i.test(ua)) {
-    return res.redirect(
-      "https://play.google.com/store/apps/details?id=com.merchant_mobile_app.paypoint&gl=UK",
-    );
+    return res.redirect(PAYPOINT_PLAY_STORE_LINK);
   }
   if (/iPhone|iPad|iPod/i.test(ua)) {
-    return res.redirect(
-      "https://apps.apple.com/gb/app/paypoint-merchant-mobile-app/id6615070817",
-    );
+    return res.redirect(PAYPOINT_APP_STORE_LINK);
   }
 
-  return res.redirect("https://associationserver.onrender.com");
+  return res.redirect("/paypoint-merchant-mobile-app");
 });
 app.use((req, res) => {
   res.status(404).send(`
